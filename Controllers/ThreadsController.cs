0d11b928fdb1c9df12dc5adea84b92a7e844d249
Alex-1611 <alex_timplarescu@yahoo.com>
2024-12-15 18:19:06 +0200

    Show Thread

---
+++ Controllers/ThreadsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using ThreadIt.Data;
+
+namespace ThreadIt.Controllers
+{
+    public class ThreadsController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public ThreadsController(ApplicationDbContext DB)
+        {
+            db = DB;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Show(int id)
+        {
+            var thr = db.Threads.Include("Comments").Include("User").Include("Comments.User").FirstOrDefault(t => t.Id == id);
+            ViewBag.thread = thr;
+            return View();
+        }
+    }
+}
